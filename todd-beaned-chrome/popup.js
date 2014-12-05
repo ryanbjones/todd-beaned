3871a763ea461e797c6a412e89eb80df099686ed
Lucas Willett & Ryan Jones <pair+ltw.ryanbjones@devbootcamp.com>
2014-12-05 15:46:51 -0600

    added chrome attempts

---
+++ todd-beaned-chrome/popup.js
@@ -0,0 +1,168 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, kittens!
+ *
+ * @type {string}
+ */
+var QUERY = 'kittens';
+
+var kittenGenerator = {
+  /**
+   * Flickr URL that will give us lots and lots of whatever we're looking for.
+   *
+   * See http://www.flickr.com/services/api/flickr.photos.search.html for
+   * details about the construction of this URL.
+   *
+   * @type {string}
+   * @private
+   */
+  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
+      'method=flickr.photos.search&' +
+      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
+      'text=' + encodeURIComponent(QUERY) + '&' +
+      'safe_search=1&' +
+      'content_type=1&' +
+      'sort=interestingness-desc&' +
+      'per_page=20',
+
+  /**
+   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
+   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
+   *
+   * @public
+   */
+  requestKittens: function() {
+    var req = new XMLHttpRequest();
+    console.log(this.searchOnFlickr_);
+    req.open("GET", this.searchOnFlickr_, true);
+    req.onload = this.showPhotos_.bind(this);
+    req.send(null);
+  },
+
+  /**
+   * Handle the 'onload' event of our kitten XHR request, generated in
+   * 'requestKittens', by generating 'img' elements, and stuffing them into
+   * the document for display.
+   *
+   * @param {ProgressEvent} e The XHR ProgressEvent.
+   * @private
+   */
+  showPhotos_: function (e) {
+    var kittens = e.target.responseXML.querySelectorAll('photo');
+    console.log(kittens);
+    for (var i = 0; i < kittens.length; i++) {
+      var img = document.createElement('img');
+      img.src = this.constructKittenURL_(kittens[i]);
+      img.setAttribute('alt', kittens[i].getAttribute('title'));
+      document.body.appendChild(img);
+    }
+  },
+
+  /**
+   * Given a photo, construct a URL using the method outlined at
+   * http://www.flickr.com/services/api/misc.urlKittenl
+   *
+   * @param {DOMElement} A kitten.
+   * @return {string} The kitten's URL.
+   * @private
+   */
+  constructKittenURL_: function (photo) {
+    return "http://farm" + photo.getAttribute("farm") +
+        ".static.flickr.com/" + photo.getAttribute("server") +
+        "/" + photo.getAttribute("id") +
+        "_" + photo.getAttribute("secret") +
+        "_s.jpg";
+  }
+};
+
+
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  kittenGenerator.requestKittens();
+});
+
+
+ // ****************************************************************************
+//                    FACEBOOK API REQUESTS
+ // ****************************************************************************
+
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    console.log(response.authResponse.accessToken);
+    accessToken = response.authResponse.accessToken;
+    userID = response.authResponse.userID;
+    testAPI();
+    $(document).trigger("fbLoaded");
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+  }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '1597302243832467',
+  cookie     : true,  // enable cookies to allow the server to access
+                      // the session
+  xfbml      : true,  // parse social plugins on this page
+  version    : 'v2.1' // See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
+$(document).on("fbLoaded",function() {
+  // Get my photos from FB
+  FB.api(
+  "me/photos",//?fields=tagged{full_picture},picture",
+  function (response) {
+    if (response && !response.error) {
+      addImagesToBody(response);
+    }
+});
+
+var getSmallestImageSrc = function(pictureObject) {
+  var lastIndex = pictureObject.length-1;
+  return pictureObject.images[lastIndex].source;
+}
+
+var addImagesToBody = function(response) {
+  for (var i = 0; i < response.data.length; i++) {
+    var src = getSmallestImageSrc(response.data[i])
+
+    var img = "<img src='"+src+"'>";
+    $("body").append(img);
+  }
+}
+
